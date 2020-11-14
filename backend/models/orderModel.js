355a04fafb6c01f893ef945c2086ce51b2e68864
bangjaehun <qkdwogns98@gmail.com>
2020-11-14 19:15:49 +0900

    solve) db delete -> new create

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -24,7 +24,7 @@ const orderSchema = mongoose.Schema(
       address: { type: String, required: true },
       city: { type: String, required: true },
       postalCode: { type: String, required: true },
-      county: { type: String, required: true },
+      country: { type: String, required: true },
     },
     paymentMethod: {
       type: String,
@@ -36,7 +36,7 @@ const orderSchema = mongoose.Schema(
       update_time: { type: String },
       email_address: { type: String },
     },
-    textPrice: {
+    taxPrice: {
       type: Number,
       required: true,
       default: 0.0,
