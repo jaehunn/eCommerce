ebd8f01513201947de383d2dfa19e2a38088430d
bangjaehun <qkdwogns98@gmail.com>
2020-12-04 20:46:16 +0900

    productscreen review...

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import morgan from "morgan";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
@@ -19,6 +20,10 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
 
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -54,7 +54,6 @@ const OrderScreen = ({ match, history }) => {
     );
   }
 
-  // TODO) PayPalButton
   useEffect(() => {
     if (!userInfo) history.push("/login");
 
